b0481d0ce9e342ebeab2d5c6eda290307fc70532
gary <garywhittle19@gmail.com>
2019-07-12 23:05:43 +0100

    Made some changes to momentum gaining, implemented ship with dynamic material ready to be set up and location of bird collision. Changed some birdpawn acceleration values too.

--- Source/SundownPrototype/Ship.cpp
+++ Source/SundownPrototype/Ship.cpp
@@ -1,11 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Ship.h"
-#include "Materials/MaterialInstanceDynamic.h"
+#include "Engine.h"
+
 
 // Sets default values
 AShip::AShip()
 {
+	//Register Events
+	OnActorBeginOverlap.AddDynamic(this, &AShip::OnOverlapBegin);
+
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -15,6 +19,10 @@ AShip::AShip()
 	//
 	ShipMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ship mesh"));
 	ShipMesh->SetupAttachment(RootComponent);
+
+	//
+	SailMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sail mesh"));
+	SailMesh->SetupAttachment(ShipMesh);
 
 	//
 	ShipSpline = CreateDefaultSubobject<USplineComponent>(TEXT("Ship spline"));
@@ -30,14 +38,13 @@ void AShip::BeginPlay()
 	ShipMesh->SetWorldLocation(ShipSpline->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World));
 	ShipMesh->SetWorldRotation(ShipSpline->GetRotationAtSplinePoint(0, ESplineCoordinateSpace::World));
 
-
 	// Create dynamic material instance
-	UMaterialInstanceDynamic* DynMaterial = UMaterialInstanceDynamic::Create(SailMaterial, this);
+	DynMaterial = UMaterialInstanceDynamic::Create(SailMaterial, this);
 	// Set parameters
-	DynMaterial->SetScalarParameterValue("Fire amount", fFireAmount);
+	DynMaterial->SetScalarParameterValue("Fire amount", 0.0f);
 
 	//
-	ShipMesh->SetMaterial(0, DynMaterial);
+	SailMesh->SetMaterial(0, DynMaterial);
 }
 
 // Called every frame
@@ -58,4 +65,12 @@ void AShip::Tick(float DeltaTime)
 	// Set the book mesh to be at the correct location and orientation
 	ShipMesh->SetWorldLocation(ShipSpline->GetLocationAtDistanceAlongSpline(fSplineProgress, ESplineCoordinateSpace::World));
 	ShipMesh->SetWorldRotation(ShipSpline->GetRotationAtDistanceAlongSpline(fSplineProgress, ESplineCoordinateSpace::World) + rCorrection);
+}
+
+//Called when character overlaps with collision box
+void AShip::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	DynMaterial->SetScalarParameterValue("Fire amount", 1.0f);
+	FVector CinderLocation = OtherActor->GetActorLocation();
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Sail on fire at: %s"), *CinderLocation.ToString()));
 }
