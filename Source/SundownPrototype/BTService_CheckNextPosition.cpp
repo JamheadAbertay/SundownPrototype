fb39865d2dbd1b267055b71a46c9995b01883f2c
Ryan Mcdonald <ryan.mcdonald.1998@outlook.com>
2019-07-12 12:01:36 +0100

    stuff changes, not me

---
+++ Source/SundownPrototype/BTService_CheckNextPosition.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTService_CheckNextPosition.h"
+#include "Engine.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
+#include "BirdPawn.h"
+#include "SeagullController.h"
+#include "Seagull.h"
+
+UBTService_CheckNextPosition::UBTService_CheckNextPosition()
+{
+	bCreateNodeInstance = true;
+}
+
+void UBTService_CheckNextPosition::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) {
+	ASeagullController *SeagullAI = Cast<ASeagullController>(OwnerComp.GetAIOwner());
+
+	if (SeagullAI) 
+	{
+		ABirdPawn *Cinder = Cast<ABirdPawn>(GetWorld()->GetFirstPlayerController()->GetPawn());
+
+		if (Cinder)
+		{
+			OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Object>(SeagullAI->SeagullKeyID, SeagullAI);
+			GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Eureka")));
+		}
+	}
+}
